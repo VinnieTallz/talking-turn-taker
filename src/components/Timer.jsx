ff74ae3f049c1f9b8c273011ff863171421b7455
kbyun-ubuntu-frrqn73 <72177353+k-byun@users.noreply.github.com>
2024-12-05 15:05:35 -0700

    git merge image loaded

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { formatTime, getPhaseText } from '../utils/timerUtils';
+import React from "react";
+import { formatTime, getPhaseText } from "../utils/timerUtils";
 
 function Timer({ time, isSpeakerTime, currentSpeaker }) {
   return (
@@ -7,6 +7,17 @@ function Timer({ time, isSpeakerTime, currentSpeaker }) {
       <div className="time">{formatTime(time)}</div>
       <div className="phase">
         {getPhaseText(isSpeakerTime, currentSpeaker)}
+        {!isSpeakerTime && (
+          <img
+            src="https://slack-imgs.com/?c=1&o1=ro&url=https%3A%2F%2Fmedia1.tenor.com%2Fm%2FD12KYBUCOBAAAAAd%2Fgit-merge.gif"
+            alt="Vite"
+            width="500"
+            height="500"
+            // style="position: relative"
+            // style="z-index:10"
+            // style="position: relative; z-index: 10;"
+          />
+        )}
       </div>
     </div>
   );
--- src/utils/timerUtils.js
+++ src/utils/timerUtils.js
@@ -3,7 +3,9 @@ export const formatTime = (seconds) => `${seconds} seconds`;
 
 export const getPhaseText = (isSpeakerTime, currentSpeaker) => {
   if (isSpeakerTime) {
-    return currentSpeaker ? `Speaker Time (${currentSpeaker}'s turn)` : 'Speaker Time';
+    return currentSpeaker
+      ? `Speaker Time (${currentSpeaker}'s turn)`
+      : "Speaker Time";
   }
-  return 'Open Discussion Time';
+  return "Open Discussion Time";
 };
